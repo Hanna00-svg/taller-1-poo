0bcc8d2029b6d594338de163c6b5828feac80a7a
Sofia <sofiarango2500@gmail.com>
2026-03-14 14:31:15 -0500

    Correccion clase vehiculo

--- src/Concesionario/Concesionario/Vehiculo.cs
+++ src/Concesionario/Concesionario/Vehiculo.cs
@@ -1,41 +1,38 @@
 using System;
+public abstract class Vehiculo : IVendible , IPersistible
 
-namespace Concesionario
-{
-    public abstract class Vehiculo : IVendible, IPersistible
     {
-
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string Marca { get; set; }
         public string Modelo { get; set; }
         public string Color { get; set; }
         public string Placa { get; set; }
+        public int Cilindraje { get; set; }
         public decimal Precio { get; set; }
         public bool Vendido { get; set; }
 
-
-
-        public Vehiculo(int id, string marca, string modelo, string color, string placa, decimal precio)
+        public Vehiculo(long id, string marca, string modelo, string color, string placa, int cilindraje, decimal precio)
         {
             Id = id;
             Marca = marca;
             Modelo = modelo;
             Color = color;
             Placa = placa;
+            Cilindraje = cilindraje;
             Precio = precio;
+            Vendido = false; 
         }
 
+        public abstract decimal CalcularPrecioFinal();
         public void Vender()
         {
             Vendido = true;
         }
 
-
-        public abstract decimal CalcularPrecioFinal();
-
         public virtual string ToCsv()
         {
-            return $"{Id},{Marca},{Modelo},{Color},{Placa},{Precio}";
+            return $"{Id},{Marca},{Modelo},{Color},{Placa},{Cilindraje},{Precio},{Vendido}";
         }
     }
-}
+
+
