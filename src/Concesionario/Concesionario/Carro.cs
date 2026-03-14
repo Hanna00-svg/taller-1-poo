cfd2a55238f154b86048539ffa01ee07fb348550
Jquirama17 <johanquirama2@gmail.com>
2026-03-14 11:12:55 -0500

    se instanciaron todas las propiedades de Carro

---
+++ src/Concesionario/Concesionario/Carro.cs
@@ -0,0 +1,10 @@
+
+public class Carro: Vehiculo
+{
+    public int Id {get;set;}   
+     public string Marca {get;set;} 
+     public string Color {get;set;} 
+      public string placa {get;set;} 
+       public int Cilindraje {get;set;} 
+        public decimal Precio {get;set;} 
+}
