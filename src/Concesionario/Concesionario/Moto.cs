5fc0fb39853782a8fe48b9c016c1b6f67fb2390d
Jquirama17 <johanquirama2@gmail.com>
2026-03-14 12:15:04 -0500

    se intanciaron las propiedades y metodos de la clase Moto

---
+++ src/Concesionario/Concesionario/Moto.cs
@@ -0,0 +1,38 @@
+public class Moto : Vehiculo
+{
+     public int Id {get;set;}   
+    public string Marca {get;set;} 
+    public string Color {get;set;} 
+    public string Placa {get;set;} 
+    public int Cilindraje {get;set;} 
+    public decimal Precio {get;set;} 
+    
+
+    public Moto(int id,string marca, string color,string placa,int cilindraje,decimal precio)
+    {
+        Id = id;
+        Marca = marca;
+        Color = color;
+        Placa = placa;
+        Cilindraje = cilindraje;
+        Precio = precio;
+        
+    }
+
+   
+    public decimal CalcularPrecioFinal()
+    {
+        decimal iva = 0;
+        if (Cilindraje >= 100 && Cilindraje <= 300)
+        {
+             iva = 0.10m;
+        }
+        if (Cilindraje > 300 && Cilindraje <=1000 )
+        {
+             iva = 0.20m;
+        }
+
+        decimal Preciofinal = Precio + iva;
+       return Preciofinal;
+    }
+}
